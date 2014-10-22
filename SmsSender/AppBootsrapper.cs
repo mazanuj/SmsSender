d3f136c6e9a2bbc275a8440d303a8565d4c4fb9d
Yura Peleh <agronom.pele@gmail.com>
2014-10-22 22:31:03 +0300

    Added Caliburn Micro

---
+++ SmsSender/AppBootsrapper.cs
@@ -0,0 +1,64 @@
+﻿﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System.Linq;
+using System.Windows;
+
+namespace SmsSender
+{
+    using Caliburn.Micro;
+
+    using SmsSender.ViewModels;
+
+    public class AppBootstrapper : BootstrapperBase
+    {
+        private CompositionContainer _container;
+
+        public AppBootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            _container =
+                new CompositionContainer(
+                    new AggregateCatalog(
+                        AssemblySource.Instance.Select(x => new AssemblyCatalog(x)).OfType<ComposablePartCatalog>()));
+            var batch = new CompositionBatch();
+            batch.AddExportedValue<IWindowManager>(new WindowManager());
+            batch.AddExportedValue<IEventAggregator>(new EventAggregator());
+            batch.AddExportedValue(_container);
+
+            _container.Compose(batch);
+        }
+
+        protected override object GetInstance(Type serviceType, string key)
+        {
+            var contract = string.IsNullOrEmpty(key) ? AttributedModelServices.GetContractName(serviceType) : key;
+            var exports = _container.GetExportedValues<object>(contract);
+
+            if (exports.Any()) return exports.First();
+
+            throw new Exception(string.Format("Could not locate any instances of contract {0}.", contract));
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type serviceType)
+        {
+            return _container.GetExportedValues<object>(AttributedModelServices.GetContractName(serviceType));
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.SatisfyImportsOnce(instance);
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<MainViewModel>();
+        }
+    }
+}
---
+++ SmsSender/ViewModels/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmsSender.ViewModels
+{
+    using System.ComponentModel.Composition;
+
+    using Caliburn.Micro;
+
+    [Export(typeof(MainViewModel))]
+    public class MainViewModel : PropertyChangedBase
+    {
+        [ImportingConstructor]
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
