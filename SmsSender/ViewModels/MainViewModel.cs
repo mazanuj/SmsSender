abf88f03aa365a644d09ba73ffcf795bf5ab0638
Yura Peleh <agronom.pele@gmail.com>
2014-10-25 01:20:57 +0300

    Work with UI

--- SmsSender/ViewModels/MainViewModel.cs
+++ SmsSender/ViewModels/MainViewModel.cs
@@ -1,32 +1,172 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SmsSender.ViewModels
+﻿namespace SmsSender.ViewModels
 {
+    using System;
+    using Caliburn.Micro;
+    using Microsoft.Win32;
     using System.ComponentModel.Composition;
     using System.Windows;
-
-    using Caliburn.Micro;
 
     [Export(typeof(MainViewModel))]
     public class MainViewModel : PropertyChangedBase
     {
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        private Byte rate = 1;
+        private bool autoStartDate;
+        private bool autoEndDate;
+        private string recipientsFile, body, source;
+        private DateTime startDate, endDate;
+
+        public DateTime StartDate
+        {
+            get
+            {
+                return startDate;
+            }
+            set
+            {
+                startDate = value;
+                GetButtonStartEnabledStatus();
+            }
+        }
+        public DateTime EndDate
+        {
+            get
+            {
+                return endDate;
+            }
+            set
+            {
+                endDate = value;
+                GetButtonStartEnabledStatus();
+            }
+        }
+        public bool AutoStartDate
+        {
+            get
+            {
+                return autoStartDate;
+            }
+            set
+            {
+                autoStartDate = value;
+                CanStartDate = !value;
+                NotifyOfPropertyChange(() => CanStartDate);
+                GetButtonStartEnabledStatus();
+            }
+        }
+        public bool AutoEndDate
+        {
+            get
+            {
+                return autoEndDate;
+            }
+            set
+            {
+                autoEndDate = value;
+                CanEndDate = !value;
+                NotifyOfPropertyChange(() => CanEndDate);
+                GetButtonStartEnabledStatus();
+            }
+        }
+        public Byte Rate
+        {
+            get
+            {
+                return rate;
+            }
+            set
+            {
+                rate = value;
+                RateLabel = value;
+                NotifyOfPropertyChange(() => RateLabel);
+            }
+        }
+        public Byte RateLabel { get; set; }
+        public string Source
+        {
+            get
+            {
+                return source;
+            }
+            set
+            {
+                source = value;
+                GetButtonStartEnabledStatus();
+            }
+        }
+        public string Body
+        {
+            get
+            {
+                return body;
+            }
+            set
+            {
+                body = value;
+                GetButtonStartEnabledStatus();
+            }
+        }
+        public bool RecipientsFileLabel { get; set; }
+        public bool CanStartDate { get; set; }
+        public bool CanEndDate { get; set; }
+        public bool CanButtonStart { get; set; }
 
         [ImportingConstructor]
         public MainViewModel()
         {
             StartDate = DateTime.Now;
             EndDate = DateTime.Now;
+
+            RateLabel = 1;
+            AutoStartDate = true;
+            AutoEndDate = true;
+        }
+
+        public void ButtonRecipients()
+        {
+            var dlg = new OpenFileDialog();
+
+            if (dlg.ShowDialog() == false) return;
+            recipientsFile = dlg.FileName;
+
+            RecipientsFileLabel = true;
+            NotifyOfPropertyChange(() => RecipientsFileLabel);
+
+            GetButtonStartEnabledStatus();
         }
 
         public void ButtonStart()
         {
-            MessageBox.Show(StartDate.ToString());
+            MessageBox.Show("!!!");
+        }
+
+        private void GetButtonStartEnabledStatus()
+        {
+            CanButtonStart = this.CheckIfAllFieldsAreFilled();
+            NotifyOfPropertyChange(() => this.CanButtonStart);
+        }
+
+        private bool CheckIfAllFieldsAreFilled()
+        {
+            if (AutoStartDate && AutoEndDate)
+                return CheckIfSomeFieldsAreFilled();
+
+            if (AutoStartDate == false && AutoEndDate == false)
+                return CheckIfSomeFieldsAreFilled() && CheckDates(StartDate, EndDate);
+
+            return CheckIfSomeFieldsAreFilled();
+        }
+
+        private bool CheckIfSomeFieldsAreFilled()
+        {
+            if (string.IsNullOrEmpty(this.Source) || string.IsNullOrEmpty(this.Body) || string.IsNullOrEmpty(this.recipientsFile))
+                return false;
+
+            return true;
+        }
+
+        private bool CheckDates(DateTime start, DateTime end)
+        {
+            return start < end;
         }
     }
 }
